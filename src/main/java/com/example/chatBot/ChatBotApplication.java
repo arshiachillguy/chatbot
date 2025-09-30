56bd96dd8eb8859980fa5258de72ef23c830057f
arshia <chillguyfromspace@gmail.com>
2025-09-30 09:25:40 +0330

    a little edit on this code block

--- src/main/java/com/example/chatBot/ChatBotApplication.java
+++ src/main/java/com/example/chatBot/ChatBotApplication.java
@@ -2,8 +2,10 @@ package com.example.chatBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.chatBot")
 public class ChatBotApplication {
 
 	public static void main(String[] args) {
